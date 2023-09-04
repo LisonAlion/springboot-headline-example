9d6d022b674bf2e1800a77fdf7086681e8a416b1
LisonAlion <106069655+LisonAlion@users.noreply.github.com>
2023-09-04 10:29:49 +0800

    1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * ClassName: ${NAME}
+ * Package:org.example
+ *
+ * @author 坚持哥
+ * @Create:${DATE}-${TIME}
+ * @version:1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
